eb93aa7b2650242559ddefb6538bbc46270e62fe
Rd27p <g.a.raka.darma.p.k@gmail,com>
2025-04-09 16:30:11 +0700

    menambahkan class team member

--- jurnalmodul7_kelompok4/Program.cs
+++ jurnalmodul7_kelompok4/Program.cs
@@ -6,6 +6,9 @@ class program
     {
         var basepath = AppContext.BaseDirectory;
         var pathData = Path.Combine(basepath, "jurnal7_1_103022300088.json");
+        var pathmem = Path.Combine(basepath, "jurnal7_2_103022300088.json");
         DataMahasiswa103022300088.ReadJSON(pathData);
+        Console.WriteLine(" ");
+        TeamMembers103022300088.ReadJson(pathmem);
     }
 }
