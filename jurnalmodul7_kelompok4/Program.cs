d0e3dc5f46bd2a61345024d0dbc4faa26adc502e
Rd27p <g.a.raka.darma.p.k@gmail,com>
2025-04-09 16:15:15 +0700

    menambahkan class datamahasiswa

--- jurnalmodul7_kelompok4/Program.cs
+++ jurnalmodul7_kelompok4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        var basepath = AppContext.BaseDirectory;
+        var pathData = Path.Combine(basepath, "jurnal7_1_103022300088.json");
+        DataMahasiswa103022300088.ReadJSON(pathData);
+    }
+}
