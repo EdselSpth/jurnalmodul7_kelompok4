0a0ed4c9126b88955591fe6495b99bad7af44ba7
RezaIndraMaulana <rezaindramaulana9@gmail.com>
2025-04-09 16:29:44 +0700

    menambahkan class TeamMember_103022300109

--- jurnalmodul7_kelompok4/Program.cs
+++ jurnalmodul7_kelompok4/Program.cs
@@ -5,5 +5,7 @@ public class main
     public static void Main(string[] args)
     { 
         DataMahasiswa_103022300109.ReadJSON();
+        Console.WriteLine(" ");
+        TeamMembers_103022300109.ReadJSON();
     }
 }
